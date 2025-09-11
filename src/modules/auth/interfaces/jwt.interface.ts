4b3d7a27260b42b7ed3d68e1f5c006942e2b5e35
TQTos <tranquangtuyen135305@gmail.com>
2025-09-11 15:43:52 +0700

    refactor(auth): update userId and courseId types to string in refresh token and JWT payload

--- src/modules/auth/interfaces/jwt.interface.ts
+++ src/modules/auth/interfaces/jwt.interface.ts
@@ -2,8 +2,8 @@ import { JWTPayload as JoseJwtPayload } from 'jose';
 import { RoleEnum } from '../../user/enums/role.enum';
 
 export interface JwtPayload extends JoseJwtPayload {
-  userId: number;
-  courseId?: number;
+  userId: string;
+  courseId?: string;
   email?: string;
   firstName?: string;
   lastName?: string;
