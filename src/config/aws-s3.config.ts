443dc54adabf21d39764c3664b57f549fd7f6f06
TQTos <tranquangtuyen135305@gmail.com>
2025-09-11 15:44:23 +0700

    refactor(course): update course ID type to string and correct example value in API property

--- src/config/aws-s3.config.ts
+++ src/config/aws-s3.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-export const awsS3Config = registerAs('awss3', () => ({
+export const awsS3Config = registerAs('awsS3', () => ({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 }));
