d8319d6be5abd6ec774e3cc8daa28c2d2ac26698
TQTos <tranquangtuyen135305@gmail.com>
2025-09-11 20:45:16 +0700

    fix(config): ensure AWS region is included in S3 configuration

--- src/config/aws-s3.config.ts
+++ src/config/aws-s3.config.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export const awsS3Config = registerAs('awsS3', () => ({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 }));
