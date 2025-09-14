cff51d1b02b20aa86c577cf5b17ae08c8035a87b
TQTos <tranquangtuyen135305@gmail.com>
2025-09-14 11:41:00 +0700

    refactor(testcases): consolidate testcase constants, remove unused DTOs, and streamline service methods

--- src/modules/problems/testcases/dto/create-testcase.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CreateTestcaseDto {
-  fileUrl: string;
-}
--- src/modules/problems/testcases/testcases.controller.ts
+++ src/modules/problems/testcases/testcases.controller.ts
@@ -1,35 +1,34 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseInterceptors,
-  UploadedFile,
-  ParseFilePipe,
-  MaxFileSizeValidator,
-  UseGuards,
-  FileTypeValidator,
   ClassSerializerInterceptor,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
-import { TestcasesService } from './testcases.service';
-import { UpdateTestcaseDto } from './dto/update-testcase.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
-import { CurrentUser } from 'src/common/decorators/current-user.decorator';
-import type { JwtPayload } from 'src/modules/auth/interfaces/jwt.interface';
 import {
   ApiCookieAuth,
   ApiOperation,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
-import { Roles } from 'src/common/decorators/roles.decorator';
-import { RoleEnum } from 'src/modules/user/enums/role.enum';
 import { plainToInstance } from 'class-transformer';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import type { JwtPayload } from 'src/modules/auth/interfaces/jwt.interface';
+import { RoleEnum } from 'src/modules/user/enums/role.enum';
+import {
+  TESTCASE_FIELD_NAME,
+  TESTCASE_FILE_MIME_TYPE,
+  TESTCASE_MAX_FILE_SIZE,
+} from './constants/testcases.constant';
 import { CreateTestcaseResponseDto } from './dto/create-testcase-response.dto';
+import { TestcasesService } from './testcases.service';
 
 @ApiTags('Testcases')
 @ApiCookieAuth('access_token')
@@ -43,13 +42,16 @@ export class TestcasesController {
   @ApiResponse({ status: 400, description: 'Bad Request.' })
   @UseGuards(JwtAuthGuard)
   @Roles(RoleEnum.INSTRUCTOR)
-  @UseInterceptors(FileInterceptor('file'), ClassSerializerInterceptor)
+  @UseInterceptors(
+    FileInterceptor(TESTCASE_FIELD_NAME),
+    ClassSerializerInterceptor,
+  )
   create(
     @UploadedFile(
       new ParseFilePipe({
         validators: [
-          new MaxFileSizeValidator({ maxSize: 10 * 1024 * 1024 }),
-          new FileTypeValidator({ fileType: 'text/plain' }),
+          new MaxFileSizeValidator({ maxSize: TESTCASE_MAX_FILE_SIZE }),
+          new FileTypeValidator({ fileType: TESTCASE_FILE_MIME_TYPE }),
         ],
       }),
     )
@@ -57,31 +59,6 @@ export class TestcasesController {
     @CurrentUser() user: JwtPayload,
   ) {
     const testcase = this.testcasesService.create(file, user);
-    return plainToInstance(CreateTestcaseResponseDto, testcase, {
-      excludeExtraneousValues: true,
-    });
-  }
-
-  @Get()
-  findAll() {
-    return this.testcasesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.testcasesService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateTestcaseDto: UpdateTestcaseDto,
-  ) {
-    return this.testcasesService.update(id, updateTestcaseDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.testcasesService.remove(id);
+    return plainToInstance(CreateTestcaseResponseDto, testcase);
   }
 }
