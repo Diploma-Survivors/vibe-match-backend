db54c15f09925aa6f19ebed7eb07054d91e46f97
TQTos <tranquangtuyen135305@gmail.com>
2025-09-21 10:44:41 +0700

    refactor(decorators): consolidate role and skip transform constants into respective decorators

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,4 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { ROLES_KEY } from '../constants/role.constant';
+
+export const ROLES_KEY = Symbol('roles');
 
 export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
