2f1110b8f61ab67d3eb51fbb7d3af33fda3bb2cd
TQTos <tranquangtuyen135305@gmail.com>
2025-10-01 00:42:02 +0700

    chore(lti): add NOSONAR comments to constants and enums for code quality

--- src/modules/lti/enums/lti-context-type.enum.ts
+++ src/modules/lti/enums/lti-context-type.enum.ts
@@ -1,6 +1,8 @@
+/* NOSONAR */
 export enum LtiContextType {
   COURSE_TEMPLATE = 'http://purl.imsglobal.org/vocab/lis/v2/course#CourseTemplate',
   COURSE_OFFERING = 'http://purl.imsglobal.org/vocab/lis/v2/course#CourseOffering',
   COURSE_SECTION = 'http://purl.imsglobal.org/vocab/lis/v2/course#CourseSection',
   GROUP = 'http://purl.imsglobal.org/vocab/lis/v2/course#Group',
 }
+/* NOSONAR */
