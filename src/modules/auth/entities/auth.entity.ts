fc8d6e95af57576b5ae01916a5c9da2c108e7467
TQTos <tranquangtuyen135305@gmail.com>
2025-09-30 23:59:33 +0700

    feat(auth, contests, courses, problems, users): update timestamp columns to use 'timestamp with time zone' for better timezone handling

--- src/modules/auth/entities/auth.entity.ts
+++ src/modules/auth/entities/auth.entity.ts
@@ -50,7 +50,10 @@ export class Auth {
     description: 'Authentication record creation timestamp',
     example: '2024-01-01T00:00:00.000Z',
   })
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({
+    type: 'timestamp with time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
   createdAt: Date;
 
   @ApiProperty({
@@ -58,7 +61,7 @@ export class Auth {
     example: '2024-01-01T00:00:00.000Z',
   })
   @Column({
-    type: 'timestamp',
+    type: 'timestamp with time zone',
     default: () => 'CURRENT_TIMESTAMP',
     onUpdate: 'CURRENT_TIMESTAMP',
   })
