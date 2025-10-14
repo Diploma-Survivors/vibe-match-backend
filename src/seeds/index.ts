b806dd0fe38227a577babb103e15cd70d171fdc4
TQTos <tranquangtuyen135305@gmail.com>
2025-10-14 23:00:54 +0700

    feat(seeds): implement problem seeding logic and integrate into runSeeds function

--- src/seeds/index.ts
+++ src/seeds/index.ts
@@ -1,4 +1,5 @@
 import { AppDataSource } from '../data-source';
+import { seedProblems } from './problem.seed';
 import { seedTags } from './tag.seed';
 import { seedTopics } from './topic.seed';
 
@@ -8,6 +9,7 @@ async function runSeeds() {
   try {
     await seedTags(dataSource);
     await seedTopics(dataSource);
+    await seedProblems(dataSource);
 
     console.log('Seeding completed successfully.');
   } catch (error) {
