4fcaf55cad843e984da547baa57fc0283522dd63
Tuyen572004 <htkt004@gmail.com>
2025-10-19 15:25:10 +0700

    chore: run lint

--- src/modules/submission/dto/submission-cursor.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Type } from 'class-transformer';
-import { IsDate } from 'class-validator';
-
-export class SubmissionCursorPayloadDto {
-  @Type(() => Date)
-  @IsDate()
-  createdAt: Date;
-}
