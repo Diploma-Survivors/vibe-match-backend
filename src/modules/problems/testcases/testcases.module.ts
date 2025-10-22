c338afe9e3f9b2bbfa77a268690b560e6dc6c04c
TQTos <tranquangtuyen135305@gmail.com>
2025-10-22 21:54:26 +0700

    feat(testcases): implement file interceptor and enhance TestcasesService with TestcaseSample repository methods

--- src/modules/problems/testcases/testcases.module.ts
+++ src/modules/problems/testcases/testcases.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoragesModule } from 'src/modules/storages/storages.module';
+import { TestcaseSample } from './entities/testcase-sample.entity';
 import { Testcase } from './entities/testcase.entity';
 import { TestcasesService } from './testcases.service';
 
 @Module({
   providers: [TestcasesService],
-  imports: [TypeOrmModule.forFeature([Testcase]), StoragesModule],
+  imports: [
+    TypeOrmModule.forFeature([Testcase, TestcaseSample]),
+    StoragesModule,
+  ],
   exports: [TestcasesService],
 })
 export class TestcasesModule {}
--- src/modules/problems/testcases/testcases.service.ts
+++ src/modules/problems/testcases/testcases.service.ts
@@ -3,9 +3,11 @@ import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import type { JwtPayload } from 'src/modules/auth/interfaces/jwt.interface';
 import { StoragesService } from 'src/modules/storages/storages.service';
-import { Repository } from 'typeorm';
+import { FindOneOptions, FindOptionsWhere, Repository } from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity.js';
 import { v4 as uuidV4 } from 'uuid';
 import { TESTCASE_FILE_EXTENSION } from './constants/testcases.constant';
+import { TestcaseSample } from './entities/testcase-sample.entity';
 import { Testcase } from './entities/testcase.entity';
 
 @Injectable()
@@ -13,6 +15,8 @@ export class TestcasesService {
   constructor(
     @InjectRepository(Testcase)
     private readonly testcaseRepository: Repository<Testcase>,
+    @InjectRepository(TestcaseSample)
+    private readonly testcaseSampleRepository: Repository<TestcaseSample>,
     private readonly storagesService: StoragesService,
     private readonly configService: ConfigService,
   ) {}
@@ -21,9 +25,12 @@ export class TestcasesService {
     file: Express.Multer.File,
     user: JwtPayload,
     problemId: number,
+    keyS3?: string,
   ) {
     const seed = uuidV4();
-    const key = `${seed}_${user.userId}_${user.courseId}${TESTCASE_FILE_EXTENSION}`;
+    const key =
+      keyS3 ??
+      `${seed}_${user.userId}_${user.courseId}${TESTCASE_FILE_EXTENSION}`;
     const bucket = this.configService.get<string>(
       'aws.s3.bucketName',
     ) as string;
@@ -36,10 +43,38 @@ export class TestcasesService {
 
     const url = this.storagesService.getObjectUrl(bucket, key);
 
+    // File exists, update record
+    if (keyS3) {
+      await this.testcaseRepository.update({ problemId }, { fileUrl: url });
+      return;
+    }
+
     const testcase = await this.testcaseRepository.save({
       fileUrl: url,
       problemId,
     });
     return testcase;
+  }
+
+  async findTestcaseOne(
+    options: FindOneOptions<Testcase>,
+  ): Promise<Testcase | null> {
+    return this.testcaseRepository.findOne(options);
+  }
+
+  async findTestcaseSamples(options: FindOneOptions<TestcaseSample>) {
+    return this.testcaseSampleRepository.find(options);
+  }
+
+  async updateTestcaseSample(
+    where: FindOptionsWhere<TestcaseSample>,
+    updateData: QueryDeepPartialEntity<TestcaseSample>,
+  ): Promise<void> {
+    await this.testcaseSampleRepository.update(where, updateData);
+  }
+
+  async createTestcaseSample(data: Partial<TestcaseSample>) {
+    const testcaseSample = this.testcaseSampleRepository.create(data);
+    return this.testcaseSampleRepository.save(testcaseSample);
   }
 }
