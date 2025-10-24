1d2573edf08a6d812508a8409eaedf6b5ffd1451
TQTos <tranquangtuyen135305@gmail.com>
2025-10-24 09:56:23 +0700

    feat(testcase-sample): update input validation to enforce string type and adjust minLength requirement

--- src/modules/problems/testcases/dto/create-testcase-sample.dto.ts
+++ src/modules/problems/testcases/dto/create-testcase-sample.dto.ts
@@ -1,17 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { MinLength } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class CreateTestcaseSampleDto {
   @ApiProperty({
     description: 'The input for the testcase',
     example: '1 2',
   })
-  @MinLength(3)
+  @IsString()
   input: string;
 
   @ApiProperty({
     description: 'The output for the testcase',
     example: '3',
+    minLength: 1,
   })
   @MinLength(1)
   output: string;
