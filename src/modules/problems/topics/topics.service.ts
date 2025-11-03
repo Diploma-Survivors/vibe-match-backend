a71d58bc765bc9c1c420e8a2cb5cf8ba7835a54f
TQTos <tranquangtuyen135305@gmail.com>
2025-11-04 00:16:35 +0700

    feat(topics): implement caching for topic operations

--- src/modules/problems/topics/topics.service.ts
+++ src/modules/problems/topics/topics.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CACHE_TTL } from 'src/common/constants/cache.constants';
+import {
+  Cacheable,
+  CacheInvalidate,
+} from 'src/common/decorators/cacheable.decorator';
 import { FindManyOptions, Repository } from 'typeorm';
 import { CreateTopicDto } from './dto/create-topic.dto';
 import { UpdateTopicDto } from './dto/update-topic.dto';
@@ -12,16 +17,22 @@ export class TopicsService {
     private readonly topicsRepository: Repository<Topic>,
   ) {}
 
+  @CacheInvalidate({ keys: ['topics:all'] })
   create(createTopicDto: CreateTopicDto) {
     const topic = this.topicsRepository.create(createTopicDto);
     return this.topicsRepository.save(topic);
   }
 
+  @CacheInvalidate({ keys: ['topics:all'] })
   async createBulk(createTopicDtos: CreateTopicDto[]) {
     const topics = this.topicsRepository.create(createTopicDtos);
     return this.topicsRepository.save(topics);
   }
 
+  @Cacheable({
+    key: 'topics:all',
+    ttl: CACHE_TTL.ONE_HOUR,
+  })
   async findAll() {
     return this.topicsRepository.find();
   }
@@ -30,14 +41,24 @@ export class TopicsService {
     return this.topicsRepository.find(options);
   }
 
+  @Cacheable({
+    key: (id: number) => `topic:${id}`,
+    ttl: CACHE_TTL.ONE_HOUR,
+  })
   async findOne(id: number) {
     return this.topicsRepository.findOne({ where: { id } });
   }
 
+  @CacheInvalidate({
+    keys: (id: number) => [`topic:${id}`, 'topics:all'],
+  })
   async update(id: number, updateTopicDto: UpdateTopicDto) {
     return this.topicsRepository.update(id, updateTopicDto);
   }
 
+  @CacheInvalidate({
+    keys: (id: number) => [`topic:${id}`, 'topics:all'],
+  })
   async remove(id: number) {
     return this.topicsRepository.delete(id);
   }
