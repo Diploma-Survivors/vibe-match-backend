4768711ccc983dd93f3e2576fd456d0fcc7add6a
TQTos <tranquangtuyen135305@gmail.com>
2025-11-04 15:18:32 +0700

    feat(problems): limit max tags and topics in CreateProblemDto

--- src/modules/problems/dto/create-problem.dto.ts
+++ src/modules/problems/dto/create-problem.dto.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Logger } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose, plainToInstance, Transform, Type } from 'class-transformer';
 import {
+  ArrayMaxSize,
   IsArray,
   IsBoolean,
   IsEnum,
@@ -122,9 +123,13 @@ export class CreateProblemDto {
     type: 'array',
     items: { type: 'int' },
     name: 'tagIds',
+    maxItems: 4,
   })
   @JsonArrayTransform('tagIds')
   @IsArray()
+  @ArrayMaxSize(4, {
+    message: 'A maximum of 4 tags can be associated with a problem',
+  })
   @IsInt({ each: true })
   @Expose({ name: 'tagIds' })
   tags: number[];
@@ -135,9 +140,13 @@ export class CreateProblemDto {
     type: 'array',
     items: { type: 'int' },
     name: 'topicIds',
+    maximum: 3,
   })
   @JsonArrayTransform('topicIds')
   @IsArray()
+  @ArrayMaxSize(3, {
+    message: 'A maximum of 3 topics can be associated with a problem',
+  })
   @IsInt({ each: true })
   @Expose({ name: 'topicIds' })
   topics: number[];
