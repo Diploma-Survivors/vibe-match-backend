a56d08168cacbb6b23aace5af572c3a86b0b9865
Tuyen572004 <htkt004@gmail.com>
2025-11-21 13:30:17 +0700

    fix missing contestId in contest-participations.entity.ts

--- src/modules/contests/entities/contest-participations.entity.ts
+++ src/modules/contests/entities/contest-participations.entity.ts
@@ -18,6 +18,9 @@ export class ContestParticipation {
   @PrimaryGeneratedColumn('increment', { name: 'contest_participation_id' })
   id: number;
 
+  @Column({ name: 'contest_id' })
+  contestId: number;
+
   @ManyToOne(() => Contest, (contest) => contest.contestParticipation)
   @JoinColumn({ name: 'contest_id' })
   contest: Contest;
