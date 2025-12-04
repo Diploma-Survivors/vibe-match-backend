698de0a3300829083598c12d7f5dbce9c956816e
TQTos <tranquangtuyen135305@gmail.com>
2025-12-04 16:42:16 +0700

    test(language): add tests for language module

--- src/modules/language/language.controller.spec.ts
+++ src/modules/language/language.controller.spec.ts
@@ -1,20 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Language } from './entities/language.entity';
 import { LanguageController } from './language.controller';
 import { LanguageService } from './language.service';
 
 describe('LanguageController', () => {
   let controller: LanguageController;
+  let service: LanguageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LanguageController],
-      providers: [LanguageService],
+      providers: [
+        {
+          provide: LanguageService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<LanguageController>(LanguageController);
+    service = module.get<LanguageService>(LanguageService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of languages', async () => {
+      const result: Language[] = [];
+      jest.spyOn(service, 'findAll').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toBe(result);
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single language', async () => {
+      const result: Language = {
+        id: 1,
+        name: 'javascript',
+      };
+      jest.spyOn(service, 'findOne').mockResolvedValue(result);
+
+      expect(await controller.findOne('1')).toBe(result);
+      expect(service.findOne).toHaveBeenCalledWith(1);
+    });
   });
 });
--- src/modules/language/language.service.spec.ts
+++ src/modules/language/language.service.spec.ts
@@ -1,18 +1,80 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Language } from './entities/language.entity';
 import { LanguageService } from './language.service';
 
 describe('LanguageService', () => {
   let service: LanguageService;
+  let repository: Repository<Language>;
+
+  const mockLanguageRepository = {
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+  };
+
+  const mockCacheManager = {
+    get: jest.fn(),
+    set: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LanguageService],
+      providers: [
+        LanguageService,
+        {
+          provide: getRepositoryToken(Language),
+          useValue: mockLanguageRepository,
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager,
+        },
+      ],
     }).compile();
 
     service = module.get<LanguageService>(LanguageService);
+    repository = module.get<Repository<Language>>(getRepositoryToken(Language));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of languages', async () => {
+      const languages = [new Language(), new Language()];
+      mockLanguageRepository.find.mockResolvedValue(languages);
+
+      const result = await service.findAll();
+
+      expect(repository.find).toHaveBeenCalled();
+      expect(result).toEqual(languages);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single language', async () => {
+      const id = 1;
+      const language = new Language();
+      mockLanguageRepository.findOneBy.mockResolvedValue(language);
+
+      const result = await service.findOne(id);
+
+      expect(repository.findOneBy).toHaveBeenCalledWith({ id });
+      expect(result).toEqual(language);
+    });
+
+    it('should throw an error if the language is not found', async () => {
+      const id = 1;
+      mockLanguageRepository.findOneBy.mockResolvedValue(null);
+
+      await expect(service.findOne(id)).rejects.toThrow(Error);
+    });
   });
 });
